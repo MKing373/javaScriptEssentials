312fd17d05c3288810a1c607b9af2d420f1ffed8
Matthew King <king.matthias@gmail.com>
2024-12-06 11:11:48 -0500

    update speed_analysis.js

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -4,6 +4,7 @@ let startTime, endTime;
 function startTest() {
     // Set the test text
     document.getElementById('inputText').value = testText;
+    document.getElementById('userInput').value = "";
 
     // Reset results and timer
     document.getElementById('output').innerHTML = "";
@@ -38,7 +39,8 @@ function endTest() {
 
     // Display the results
     var outputDiv = document.getElementById('output');
-    outputDiv.innerHTML = "<h2>Typing Test REsults:</h2>" + 
+    outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" + 
+        "<p>Total Lenght: " + userTypedText.length + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" + 
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + "seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
